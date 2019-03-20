e6e3760111c3cd6091c877d2ce215afb4b7c2d4b
Pratikchha <pdhungan@gmu.edu>
2019-03-20 19:30:40 -0400

    Item class

---
+++ GameStore/src/Item.java
@@ -0,0 +1,50 @@
+//This is a DDC for Item class
+public abstract class Item {
+	private String name;
+	private double price;
+	private String date;
+	private int numItems;
+	
+	public Item(String name, double price, String date)
+	{
+		if(name == null || name.equals(""))
+			throw new IllegalArgumentException("Name cannot be null");
+		if(price<=0)
+			throw new IllegalArgumentException("Price cannot be zero or less");
+		if(date == null || date.equals(""))
+			throw new IllegalArgumentException("Name cannot be null");
+		this.name = name;
+		this.price = price;
+		this.date = date;
+		numItems++;
+	}
+	public String getName() {return this.name;}
+	public double getPrice() {return this.price;}
+	public String getDate() {return this.date;}
+	public int getNumItems() {return this.numItems;}
+	
+	public void setName(String name)
+	{
+		if(name == null || name.equals(""))
+			throw new IllegalArgumentException("Name cannot be null");
+		this.name = name;
+	}
+	public void setPrice(double price)
+	{
+		if(price<=0)
+			throw new IllegalArgumentException("Price cannot be zero or less");
+		this.price = price;
+	}
+	public void setDate(String date)
+	{
+		if(date == null || date.equals(""))
+			throw new IllegalArgumentException("Date cannot be null");
+		this.date = date;
+	}
+	public String toString()
+	{
+		return "Item name: " + this.getName() + "\n Item price: " + this.getPrice() + "\n Date added: " + this.getDate(); 
+	}
+
+}
+

ec7638ba6bcbe6ac6633f4607227ea1f5f2578df
Pratikchha <pdhungan@gmu.edu>
2019-04-29 10:41:12 -0400

    Done

--- GameStore/src/UserTest.java
+++ GameStore/src/UserTest.java
@@ -11,11 +11,13 @@ class UserTest {
 	@Test
 	void test() {
 		User test = new User();
+		Item aItem = new Hardware("Wire", 100.02, "12/20/2018", false);
 		//setting values for User object
 		test.setFirstName("Tom");
 		test.setLastName("Jerry");
 		test.setAddress("123 Test Rd");
 		test.setPhoneNumber("5712335434");
+		test.setItem(aItem);
 		
 		// test the getFirstName of the User class
 		assertEquals("getFirstName should return the first name of the User", "Tom", test.getFirstName());
@@ -26,6 +28,24 @@ class UserTest {
 		// testing the toString method
 		assertEquals("getPhoneNumber should return the phone number of the user ", "5712335434", test.getPhoneNumber());
 
+		//testing the getItem of the User class
+		assertEquals(aItem, test.getItem(0));
+		
+		//testing the size of the Item array
+		assertEquals(1,test.getNumItems());
+		
+		//testing the user if of the user
+		assertEquals(1,test.getUserID());
+		
+		//testing the toString method of User class
+		assertEquals("toString should return the string representation of user ","\n ID : 1"
+		 + "\n First Name: Tom"
+		 + "\n Last Name: Jerry"
+		 + "\n Address: 123 Test Rd"
+		 + "\n PhoneNumber: 5712335434"+ "\n  ------------------" +
+		 "\n Item number "+ (1) +"\n" + "Wire; 100.02; 12/20/2018; false" +
+		 "\n  ------------------", test.toString());
+	
 	}
 
 }
